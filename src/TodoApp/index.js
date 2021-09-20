32256125acf28294a12186429326745c81a2c583
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-09-20 13:06:33 +0530

    added infinite scroll

--- src/TodoApp/index.js
+++ src/TodoApp/index.js
@@ -6,7 +6,7 @@ import React, {
   useState,
 } from 'react';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {ScrollView, ImageBackground, View} from 'react-native';
+import {ImageBackground, View, ActivityIndicator} from 'react-native';
 import styles from './styles';
 import {ThemeContext} from '../context/themeContext';
 import Typography from '../components/Typography';
@@ -26,41 +26,52 @@ const validateTodoForm = values => {
   }
   return errors;
 };
+const limit = 20;
 
 const TodoApp = () => {
   const {colors} = useContext(ThemeContext);
   const [todoList, setTodoList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
   const formikRef = useRef(null);
   const insets = useSafeAreaInsets();
 
-  const loadData = useCallback(async (filterType = 'all') => {
+  const loadData = useCallback(async (nextPage, filterType = 'all') => {
     try {
-      let params = {};
+      setLoading(true);
+      let params = {
+        _page: nextPage,
+        _limit: limit,
+      };
       if (filterType !== 'all') {
         params = {
-          isDone: filterType === 'completed',
+          ...params,
+          completed: filterType === 'completed',
         };
       }
-      const res = await axiosInstance.get('todoList', {
+      const res = await axiosInstance.get('todos', {
         params,
       });
-      setTodoList(res.data);
+      setTodoList(val => [...val, ...res.data]);
+      setLoading(false);
     } catch (error) {
       formikRef.current.setErrors({
         serverError: error.message,
       });
+      setLoading(false);
     }
   }, []);
 
   useEffect(() => {
-    loadData();
-  }, [loadData]);
+    loadData(page);
+  }, [page, loadData]);
 
   const toggleTodo = async (item, checked) => {
     try {
-      const res = await axiosInstance.put(`todoList/${item.id}`, {
+      const res = await axiosInstance.put(`todos/${item.id}`, {
         ...item,
-        isDone: checked,
+        completed: checked,
       });
       setTodoList(val => {
         const index = val.findIndex(x => x.id === item.id);
@@ -68,21 +79,21 @@ const TodoApp = () => {
       });
     } catch (error) {
       formikRef.current.setErrors({
-        serverError: `unable to update task "${item.text}"`,
+        serverError: `unable to update task "${item.title}"`,
       });
     }
   };
 
   const deleteTodo = async item => {
     try {
-      await axiosInstance.delete(`todoList/${item.id}`);
+      await axiosInstance.delete(`todos/${item.id}`);
       setTodoList(val => {
         const index = val.findIndex(x => x.id === item.id);
         return [...val.slice(0, index), ...val.slice(index + 1)];
       });
     } catch (error) {
       formikRef.current.setErrors({
-        serverError: `unable to delete task "${item.text}"`,
+        serverError: `unable to delete task "${item.title}"`,
       });
     }
   };
@@ -90,9 +101,9 @@ const TodoApp = () => {
   const onSubmit = async (values, actions) => {
     console.log(actions);
     try {
-      const res = await axiosInstance.post('todoList', {
-        text: values.todoText,
-        isDone: false,
+      const res = await axiosInstance.post('todos', {
+        title: values.todoText,
+        completed: false,
       });
       setTodoList(val => [...val, res.data]);
       actions.resetForm();
@@ -107,7 +118,7 @@ const TodoApp = () => {
     return (
       <View style={[globalStyles.row, globalStyles.vCenter, {margin: 10}]}>
         <Checkbox
-          checked={item.isDone}
+          checked={item.completed}
           onChange={checked => toggleTodo(item, checked)}
         />
         <Typography
@@ -117,14 +128,29 @@ const TodoApp = () => {
             globalStyles.flex,
             {
               paddingHorizontal: 8,
-              textDecorationLine: item.isDone ? 'line-through' : 'none',
+              textDecorationLine: item.completed ? 'line-through' : 'none',
             },
           ]}>
-          {item.text}
+          {`${item.id} ${item.title}`}
         </Typography>
         <BorderlessButton onPress={() => deleteTodo(item)}>
           <DeleteIcon height={24} width={24} fill={colors.primary} />
         </BorderlessButton>
+      </View>
+    );
+  };
+
+  const onEndReached = useCallback(() => {
+    console.warn('onEndReached');
+    setPage(val => val + 1);
+  }, []);
+
+  const footerComponent = () => {
+    if (loading) return null;
+
+    return (
+      <View style={globalStyles.vCenter}>
+        <ActivityIndicator color="red" size="large" />
       </View>
     );
   };
@@ -191,8 +217,8 @@ const TodoApp = () => {
         data={todoList}
         renderItem={renderItem}
         keyExtractor={item => `${item.id}`}
-        removeClippedSubviews
-        windowSize={11}
+        onEndReached={onEndReached}
+        ListFooterComponent={footerComponent}
       />
       <View
         style={[
